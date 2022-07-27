af4ed8b6af5666d1c033d463d010569deab20cd4
geetechlab <32708405+Gerard-007@users.noreply.github.com>
2022-07-27 13:40:51 -0700

    initial commit

--- ascons/urls.py
+++ ascons/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('api/contacts/', include(('contacts.api.urls', 'contacts'), namespace='contacts_api')),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+
 urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name='404.html'))]
